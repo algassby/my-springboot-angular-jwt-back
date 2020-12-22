1e1c8ddea288785285c86f887c9899b02142c0ce
algassby <algassby@hotmail.com>
2020-12-22 17:31:26 +0100

    my project config

---
+++ src/test/java/com/barry/spring/MySpringBootJwtAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barry.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootJwtAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
